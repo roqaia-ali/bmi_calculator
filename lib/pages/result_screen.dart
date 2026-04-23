c4214050cd8109cfd33c52605288960d161cbc42
roqaia-ali <roqaiaali777@gmail.com>
2026-04-24 01:24:44 +0300

    bmi_calculate_repo

--- lib/pages/result_screen.dart
+++ lib/pages/result_screen.dart
@@ -41,6 +41,7 @@ class ResultScreen extends StatelessWidget {
          child: Padding(
            padding: const EdgeInsets.all(15.0),
            child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.start,
            
             children: [
@@ -54,12 +55,12 @@ class ResultScreen extends StatelessWidget {
                   children: [
                     Padding(padding: const EdgeInsets.all(15.0),),
                     Text(getCategery(),style: TextStyle(color: Colors.green,fontSize: 20),),
-                    SizedBox(height: 100,),
+                    //SizedBox(height: 100,),
                      Text(
                         result.toStringAsFixed(2),
                         style: TextStyle(fontSize: 35,color: Colors.white),
                       ),
-                        SizedBox(height: 100),
+                        //SizedBox(height: 100),
                       Padding(
                         padding: const EdgeInsets.symmetric(horizontal: 30),
                         child: Text("your body wieght is absoltle ${getCategery()}",//textAlign: TextAlign.center,
